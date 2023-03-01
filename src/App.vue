25cb63f38478cf402d79d19a66cf91bfbf927e00
ADMIN\Admin <hau@wacontre.com>
2023-03-01 17:31:51 +0700

    todo-vue 20230301_1731

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 </style>
 
 <template>
+    <TaskItem contentIntroduce='Hi I am the child component' />
     <div class="container">
         <div class="py-5">
             <div class="row align-items-start">
@@ -15,11 +16,29 @@
                     </form>
                 </div>
                 <div class="col-8">
-                    <ul class="list-group">
-                        <li class="list-group-item" v-for="({ title, created_at }, index) in tasks">
-                            {{ title }} 
-                        </li>
-                    </ul>
+                    <table class="table table-bordered border-primary">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="({ id, title, created_at }, index) in tasks">
+                                <td>{{ id }}</td>
+                                <td>{{ title }}</td>
+                                <td>
+                                    <button type="button" class="btn btn-light me-2">
+                                        <img src="./assets/images/v1/icons/edit.png" width="24" height="24"/>
+                                    </button>
+                                    <button type="button" class="btn btn-light">
+                                        <img src="./assets/images/v1/icons/delete.png" width="24" height="24"/>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
         </div>
         </div>
@@ -29,11 +48,15 @@
 
 
 <script type="text/javascript">
+
+import TaskItem from './components/TaskItem.vue'
+
 export default {
     data() {
         return {
             tasks: [
                 {
+                    id: 1,
                     title: 'Learning English',
                     status: '',
                     created_at: '',
@@ -42,8 +65,13 @@ export default {
             ],
             task: {
                 title: '',
-            }
+            },
+            question: '',
+            answer: 'Questions usually contain a question mark. ;-)'
         }
+    },
+    components: {
+        TaskItem
     },
     methods: {
         createTask(event) {
@@ -55,10 +83,28 @@ export default {
                 isComplete: false,
             });
             this.tasks = newTasks;
+        },
+        async getAnswer() {
+            this.answer = 'Thinking...'
+            try {
+                const res = await fetch('https://yesno.wtf/api')
+                this.answer = (await res.json()).answer
+            } catch (error) {
+                this.answer = 'Error! Could not reach the API. ' + error
+            }
         }
     },
-    mounted() {
-
+    watch: {
+        // Whenever 'task.title' changes, this function will run
+        // The watch option also supports a dot-delimited path as the key
+        'task.title'(newTitle, oldTitle) {
+            if (newTitle.includes('?')) {
+                this.getAnswer();
+            }
+        }
+    },
+    mounted() { 
+        console.log(`the component is now mounted.`);
     }
 }
 </script>
---
+++ src/components/TaskItem.vue
@@ -0,0 +1,19 @@
+<style lang="scss">
+
+</style>
+
+<template>
+    {{ contentIntroduce }}
+</template>
+
+<script type="text/javascript">
+export default {
+    data() {
+
+    },
+    methods: {
+
+    },
+    props: ['contentIntroduce']
+}
+</script>
